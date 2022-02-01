8f97c9f7f46ad02891e6ad128ebfc130f4d11630
Balogun Abdulrahman <balogunabdulrahman@Baloguns-MBP.lan>
2022-02-01 13:52:33 +0100

    first commit made 01-02-2022 1:52pm

---
+++ components/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-red-500 text-center">Hello Bolaji akindele</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <HomePage  class="text-center"/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
