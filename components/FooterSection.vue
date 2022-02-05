f0538822a2dbbbefad0ecffe62a143892ee8e260
MohBorLarge <shittu.mobolaji.7@gmail.com>
2022-02-05 23:31:02 +0100

    updated footer 05/02/2022 11:30pm

---
+++ components/FooterSection.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="bg-black text-white py-28">
+      <div class="container">
+        <div class="text-6xl mb-8 font-bold">
+          <p class="mb-4">Not only solve your problem</p>
+          <p>We reframe your story</p>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente,
+            soluta?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="bg-white text-black pt-40">
+      <div class="flex justify-between container">
+        <div>
+          <p>Find Us</p>
+          <p>icons</p>
+        </div>
+        <div class="flex">
+          <div class="card bg-blue-500 p-8 w-60 text-white mr-8">
+            <div class="bg-blue-300 w-10 h-10 text-center mb-24">a</div>
+            <p class="text-xl mb-2">Book a House</p>
+            <p>Lorem ipsum dolor sit.</p>
+            <p>Lorem ipsum dolor sit.</p>
+          </div>
+          <div class="card bg-blue-500 p-8 w-60 text-white">
+            <div class="bg-blue-300 w-10 h-10 text-center mb-24">a</div>
+            <p class="text-xl mb-2">Book a House</p>
+            <p>Lorem ipsum dolor sit.</p>
+            <p>Lorem ipsum dolor sit.</p>
+          </div>
+        </div>
+      </div>
+      <div class="container flex justify-between pt-12">
+        <div class="space-x-5">
+              <a href="" class="underline">Term of use</a>
+              <a href="" class="underline">Privacy Policy</a>
+              <a href="" class="underline">Cookie settings</a>
+        </div>
+        <div class="">
+          <p>Copyright © 2022 RightSource. All Right Reserved.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterSection',
+  data() {},
+}
+</script>
---
+++ components/HeaderBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header class="flex items-center mt-8 mb-12 justify-between">
+      <img src="@/assets/RightStayLogo/svg/bluegreen.svg" alt="" class="w-40" />
+      <ul class="w-2/5 flex justify-between">
+        <li class="cursor-pointer">
+          <a :href="headerLinks.homeType">Home Type</a>
+        </li>
+        <li class="cursor-pointer">About Us</li>
+        <li class="cursor-pointer">Contact</li>
+        <li class="cursor-pointer">Blog</li>
+        <li>Buy Property</li>
+      </ul>
+    </header>
+  </div>
+</template>
+<script>
+  export default {
+    name: "HeaderBar",
+    data() {
+      return {
+        headerLinks: {
+          homeType: "https://github.com/johnpapa/vue-getting-started"
+        }
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
