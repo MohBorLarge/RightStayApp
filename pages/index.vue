bab8997bc558ba65827606acde39b5fd3bef0c2c
MohBorLarge <shittu.mobolaji.7@gmail.com>
2022-02-07 10:11:03 +0100

    reviewing cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
     <HeroCard />
-    <div class="mt-20 mb-24">
+    <div class="mt-20 mb-24 flex justify-between">
       <p class="w-64 text-2xl">We are already all around Lagos.</p>
+      <button class="w-36 h-12 bg-gradient-to-r from-green-200 to-green-300 text-white rounded">View All Houses</button>
     </div>
-    <div class="text-center text-2xl my-12 mx-auto">
+    <HouseView />
+    <!-- <div class="text-center text-2xl mb-12 mx-auto">
       <a href="#" class="text-green-400 hover:text-green-700"
         >View All Houses</a
       >
-    </div>
-    <HouseView />
+    </div> -->
   </div>
 </template>
 
